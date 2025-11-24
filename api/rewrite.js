f41ffa8313c6f3788a0295de1582b5eb03d0fc2f
benjaminruane <benjamin@ruane.co.nz>
2025-11-24 13:40:00 +0800

    Update rewrite.js

--- api/rewrite.js
+++ api/rewrite.js
@@ -105,11 +105,12 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  if (req.method !== "POST") {
+   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-    try {
+  try {
+
     const {
       text,
       notes,
