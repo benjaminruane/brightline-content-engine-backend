de4c3d8c27f01939c1eaf5962507479656c2017c
benjaminruane <benjamin@ruane.co.nz>
2025-11-24 14:31:23 +0800

    Update template.js

--- helpers/template.js
+++ helpers/template.js
@@ -1,8 +1,14 @@
 // helpers/template.js
 
-export function fillTemplate(template, vars) {
-  return Object.entries(vars).reduce((acc, [key, value]) => {
-    const safe = value == null ? "" : String(value);
-    return acc.replaceAll(`{{${key}}}`, safe);
-  }, template);
+// Very simple token replacement: {{title}}, {{notes}}, {{text}}, {{scenario}}
+export function fillTemplate(template, values) {
+  let result = String(template);
+
+  const map = values || {};
+  Object.keys(map).forEach(function (key) {
+    const pattern = new RegExp("\\{\\{" + key + "\\}\\}", "g");
+    result = result.replace(pattern, map[key] == null ? "" : String(map[key]));
+  });
+
+  return result;
 }
