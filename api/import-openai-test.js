6dbe9eb12ff4424d3ec03ee269788e5a0e1bdb45
benjaminruane <benjamin@ruane.co.nz>
2026-01-04 13:48:35 +0800

    Create import-openai-test.js

---
+++ api/import-openai-test.js
@@ -0,0 +1,10 @@
+// api/import-openai-test.js
+import OpenAI from "openai";
+
+export default async function handler(req, res) {
+  res.status(200).json({
+    ok: true,
+    imported: true,
+    openaiType: typeof OpenAI,
+  });
+}
