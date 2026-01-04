fbf836822bc2a43bd1c003d205d1e4a6c1c77090
benjaminruane <benjamin@ruane.co.nz>
2026-01-04 10:23:45 +0800

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -27,6 +27,11 @@ function setCorsHeaders(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 }
+
+export default async function handler(req, res) {
+  setCorsHeaders(req, res);
+  if (req.method === "OPTIONS") return res.status(200).end();
+  
 // ------------------------------------------------------------------
 
 const STYLE_GUIDE_INSTRUCTIONS = `
